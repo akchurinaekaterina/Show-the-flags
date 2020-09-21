8caf3c91931da4cc0b8103e0a47b26dd11d54530
Ekaterina Akchurina <akchurina.ekaterina@gmail.com>
2020-09-21 20:04:33 +0300

    Initial Commit

---
+++ Show the flags/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Show the flags
+//
+//  Created by Ekaterina Akchurina on 21.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
