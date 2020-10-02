f6a99c162a189c00f674cd9dfe9fdad35ebecdee
Ekaterina Akchurina <akchurina.ekaterina@gmail.com>
2020-10-02 20:09:55 +0300

    added number of opening (challenge from day 49)

--- Show the flags/ViewController.swift
+++ Show the flags/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 class ViewController: UITableViewController {
     
     var countries = [String]()
+    
+    var countofLooking = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         loadImages()
         title = "Here are the flags"
+        
+        loadCount()
         // Do any additional setup after loading the view.
     }
     
@@ -45,13 +49,37 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
         cell.imageView?.image = UIImage(named: countries[indexPath.row])
-        cell.textLabel?.text = countries[indexPath.row]
+        cell.textLabel?.text = "\(countries[indexPath.row]), seen: \(countofLooking[indexPath.row]) "
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let dc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
             dc.imagename = countries[indexPath.row]
+            saveCountForPicture(indexPath.row)
+            tableView.reloadData()
             navigationController?.pushViewController(dc, animated: true)
+        }
+    }
+    func loadCount(){
+        let defaults = UserDefaults.standard
+        if let loadNumbers = defaults.data(forKey: "seen") {
+            let jsonDecoder = JSONDecoder()
+            guard let loadedData = try? jsonDecoder.decode([Int].self, from: loadNumbers) else {fatalError("Unable to decode")}
+            countofLooking = loadedData
+        } else {
+            for country in countries {
+                countofLooking.append(0)
+            }
+        }
+    }
+    func saveCountForPicture (_ item: Int) {
+        let defaults = UserDefaults.standard
+        countofLooking[item] += 1
+        let jsonEncoder = JSONEncoder()
+        if let encoded = try? jsonEncoder.encode(countofLooking) {
+            defaults.set(encoded, forKey: "seen")
+        } else {
+            fatalError("Unable to encode")
         }
     }
 
